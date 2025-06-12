14d7d94b5e4e71bbfb2bf83fa7daae8b2f8ac7d7
Niyatizzz <niyatikolukula@gmail.com>
2025-06-12 16:21:33 +0530

    dashboard route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Home";
+import HackTopiaDashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/dashboard" element={<HackTopiaDashboard />} />
         {/* Add other routes here, for example:
         <Route path="/about" element={<About />} />
         */}
